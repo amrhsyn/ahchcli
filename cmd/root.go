2dfa5c90efb10d1b5c4939ceb404c73963c4eea3
“Amirhossein” <amirhossein@justdice.io>
2023-04-21 14:56:55 +0200

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "my test cli",
+	Short: "this is just a test cli",
+	Long:  `this is just a test clik long long long `,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		flagVal, err := cmd.Flags().GetBool("ahch")
+		if err != nil {
+			return
+		}
+		if flagVal {
+			fmt.Println("Hello world alternative")
+			return
+		}
+		fmt.Println("hello world")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.myapp.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("ahch", "a", false, "for ahch")
+}
